cc6311f583a312b2c3df6de90cb56761e16bb3be
Bogdan0075 <vasaboda014@gmail.com>
2022-11-08 10:57:43 +0200

    Change like and dislike function

--- index.js
+++ index.js
@@ -86,10 +86,10 @@ function setNewPhoto(url) {
     window.localStorage.length,
     JSON.stringify({
       name: document.querySelector(".inputName").value,
-      name2: "something",
+      name2: document.querySelector(".description").value,
       like: false,
       dislike: false,
-      count_likes: 0,
+      count_like: 0,
       count_dislike: 0,
       url: url,
       comments: [],
@@ -109,34 +109,46 @@ function openModal() {
       modal.classList.remove("is-hidden");
     });
   }
-
   closeModal();
 }
 
+let id;
+let data;
+let countLike = document.querySelector(".count_of_like");
+let countDislike = document.querySelector(".count_of_dislike");
+
 function getPhotoData(img) {
   popupImage.src = img.src;
-  const data = JSON.parse(window.localStorage.getItem(img.id));
+  id = img.id;
+  data = JSON.parse(window.localStorage.getItem(img.id));
   document.querySelector("#name").textContent = data.name;
   document.querySelector("#description").textContent = data.name2;
+  countLike.innerHTML = data.count_like;
+  countDislike.innerHTML = data.count_dislike;
   likeAndDislike(data);
+  createMarkupForComments();
 }
+
 function closeModal() {
   let closeModal = document.querySelector(".modal-btn-close");
   closeModal.addEventListener("click", () => {
     const btn1 = document.querySelector(".like__block");
     const btn2 = document.querySelector(".dislike__block");
-    btn1.removeEventListener("click", like);
-    btn2.removeEventListener("click", dislike);
+    btn1.removeEventListener("click", () => {});
+    btn2.removeEventListener("click", () => {});
     modal.classList.add("is-hidden");
     modalParams.classList.add("is-hidden");
+    clearComments();
   });
 }
+//Механіка лайків
 const btn1 = document.querySelector(".like__block");
 const btn2 = document.querySelector(".dislike__block");
 const btn_like = document.querySelector(".like");
 const btn_dislike = document.querySelector(".dislike");
-//Механіка лайків
+
 function likeAndDislike(img) {
+  console.log(img.like);
   if (img.like) {
     btn_like.classList.add("green");
   } else {
@@ -148,161 +160,67 @@ function likeAndDislike(img) {
     btn_dislike.classList.remove("red");
   }
 
-  btn1.addEventListener("click", like(img));
+  btn1.addEventListener("click", likes);
 
-  btn2.addEventListener("click", dislike(img));
+  btn2.addEventListener("click", dislikes);
 }
-function like(img) {
+
+function likes() {
   if (btn_dislike.classList.contains("red")) {
     btn_dislike.classList.remove("red");
-    img.count_dislike--;
-    img.dislike = false;
+    data.count_dislike--;
+    data.dislike = false;
+    countDislike.innerHTML--;
   }
   btn_like.classList.toggle("green");
   if (btn_like.classList.contains("green")) {
-    img.like = true;
-    img.count_like++;
+    data.like = true;
+    data.count_like++;
+    countLike.innerHTML++;
   } else {
-    img.like = false;
-    img.count_like--;
+    data.like = false;
+    data.count_like--;
+    countLike.innerHTML--;
   }
+  updatePhotoData();
 }
 
-function dislike(img) {
+function dislikes() {
   if (btn_like.classList.contains("green")) {
     btn_like.classList.remove("green");
-    img.count_like--;
-    img.like = false;
+    data.count_like--;
+    data.like = false;
+    countLike.innerHTML--;
   }
   btn_dislike.classList.toggle("red");
   if (btn_dislike.classList.contains("red")) {
-    img.dislike = true;
-    img.count_dislike++;
+    data.dislike = true;
+    data.count_dislike++;
+    countDislike.innerHTML++;
   } else {
-    img.dislike = false;
-    img.count_dislike--;
+    data.dislike = false;
+    data.count_dislike--;
+    countDislike.innerHTML--;
   }
-  console.log("Кількість дизлайків" + img.count_dislike);
+  updatePhotoData();
+}
+
+function updatePhotoData() {
+  window.localStorage.setItem(id, JSON.stringify(data));
+}
+
+// Функції для роботи з коментарями
+function createMarkupForComments() {
+  const comments_list = document.querySelector(".comment_list");
+  const markupForComments = data.comments
+    .map((coment) => `<li>${coment}</li>`)
+    .join("");
+
+  comments_list.insertAdjacentHTML("beforeend", markupForComments);
+}
+
+function clearComments() {
+  const comments_list = document.querySelector(".comment_list");
+  comments_list.innerHTML = "";
 }
 SetDatabase();
-
-// let colection = document.querySelector("#app");
-// let modal = document.querySelector(".modal");
-// let modalParams = document.querySelector(".popupSizeParams");
-// let popupImage = document.querySelector("#popupImage");
-// const button = document.querySelector(".button");
-
-// async function SetDatabase() {
-//   const data = await fetch(`./items.json`, {
-//     method: "GET",
-//   }).then((res) => {
-//     return res.json();
-//   });
-//   for (const [index, item] of data.entries()) {
-//     window.localStorage.setItem(index, JSON.stringify(item));
-//   }
-//   createMarkup();
-//   openModal();
-// }
-
-// function getData() {
-//   let arr = [];
-//   for (let i = 0; i < window.localStorage.length; i++) {
-//     arr.push(JSON.parse(window.localStorage.getItem(i)));
-//   }
-//   return arr;
-// }
-
-// function createMarkup() {
-//   let arrayData = getData();
-
-//   const markup = arrayData
-//     .map(
-//       (photo) =>
-//         `<div><img class="data-modal-open" id="${photo.name}" src="${photo.url}" alt=""></div>`
-//     )
-//     .join("");
-
-//   colection.insertAdjacentHTML("beforeend", markup);
-//   createAddBlock();
-// }
-// formValidate();
-
-// function createAddBlock() {
-//   const photoAdd = document.createElement("div");
-//   photoAdd.style.backgroundImage = 'url("./photos/background.png")';
-//   photoAdd.classList.add("add");
-//   photoAdd.addEventListener("click", () => {
-//     modalForm.classList.remove("is-hidden");
-//     let inputName = document.querySelector(".inputName");
-//     inputName.value = "";
-//     inputName.addEventListener("input", formValidate);
-//   });
-
-//   colection.appendChild(photoAdd);
-//   createBlockOnClick();
-// }
-
-// function createBlockOnClick() {
-//   const button = document.querySelector(".button");
-//   const modalForm = document.querySelector("#modalForm");
-//   modalForm.addEventListener("click", (event) => {
-//     if (event.target == modalForm) {
-//       modalForm.classList.add("is-hidden");
-//     }
-//   });
-//   button.addEventListener("click", () => {
-//     if (button.disabled) {
-//       Notiflix.Notify.failure("Qui timide rogat docet negare");
-//     }
-//   });
-// }
-
-// function setNewPhoto(url) {
-//   window.localStorage.setItem(
-//     window.localStorage.length,
-//     JSON.stringify({
-//       name: document.querySelector(".inputName").value,
-//       name2: "something",
-//       count_likes: "121",
-//       count_dislike: "134",
-//       url: url,
-//       comments: [],
-//     })
-//   );
-//   colection.innerHTML = "";
-//   createMarkup();
-//   openModal();
-// }
-
-// function openModal() {
-//   let arrImages = document.querySelectorAll(".data-modal-open");
-//   console.log(arrImages);
-//   for (let img of arrImages) {
-//     img.addEventListener("click", () => {
-//       popupImage.src = img.src;
-//       document.querySelector("#name").textContent = "Some";
-//       modalParams.classList.remove("is-hidden");
-//       modal.classList.remove("is-hidden");
-//     });
-//   }
-//   closeModal();
-// }
-// function closeModal() {
-//   let closeModal = document.querySelector(".modal-btn-close");
-//   closeModal.addEventListener("click", () => {
-//     modal.classList.add("is-hidden");
-//     modalParams.classList.add("is-hidden");
-//   });
-// }
-
-// function formValidate() {
-//   let nameInput = document.querySelector(".inputName");
-//   if (nameInput.value === "") {
-//     button.disabled = true;
-//   } else {
-//     button.disabled = false;
-//   }
-// }
-
-// SetDatabase();
